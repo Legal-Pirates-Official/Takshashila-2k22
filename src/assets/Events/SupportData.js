57a0da2ffdef16a6d621755be98644dab110b7f5
s-ashwin <ashwin.sivanagarajan@gmail.com>
2022-05-08 20:34:55 +0530

    Updates Support Contact Numbers

--- src/assets/Events/SupportData.js
+++ src/assets/Events/SupportData.js
@@ -1,4 +1,4 @@
 export const SupportData = `In case of any issues with registration or website you can contact us at
 takshashila@citchennai.net
-General Queries (+91 9894107330)
-Events (+91 9245499401)`
+General Queries (+91 94449 88873)
+Events (+91 90809 68635)`
