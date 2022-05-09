5fa993c86a99ae036f3a8fc787c66335a2a2150d
s-ashwin <ashwin.sivanagarajan@gmail.com>
2022-05-09 10:39:11 +0530

    Updates partial content

--- src/assets/Events/CulturalEventsData.js
+++ src/assets/Events/CulturalEventsData.js
@@ -3,114 +3,91 @@ export const culturalEventsData = [
     heading: "Pro Shows",
     data: [
       {
-        name: "event1",
+        name: "Choreo Night",
         description:
-          "A photo is a moment frozen forever in time. Show the life you've lived through the lens and the moments you've preserved through your clicks.",
+          "Spike up your dancing skills that's rolled up under your sleeves as the music grooves. Come and show off your moves with your crew!!.  A photo is a moment frozen forever in time. Show the life you've lived through the lens and the moments you've preserved through your clicks.",
         rules: [
           {
             Prelims: [
-              "The work will be judged based on creativity, incase of tie likes will      be considered.",
-              "The entries shouldn’t have any watermark.",
-              " The entries should be submitted during registration.",
-              "Identification of plagiarism will lead to disqualification",
-              " The judge's decision will be final"
+              "The google forms have to be filled and the video can be uploaded through the same.",
+              "Visit our website link www.cittakshashila.org for uploading. ",
+              "Only a single non-edited video should be submitted. ",
+              "The maximum duration of the video should be 3 minutes.",
+              "The last day for the submission of the video is before the 14th of May 2022."
             ]
           },
           {
-            "Final": [
-              "The work will be judged based on creativity, incase of tie likes will      be considered.",
-              "The entries shouldn’t have any watermark.",
-              " The entries should be submitted during registration.",
-              "Identification of plagiarism will lead to disqualification",
-              " The judge's decision will be final"
+            Finals: [
+              "The maximum time restriction is 7 minutes. ",
+              "Cross-over between teams is permitted. ",
+              "Vulgarity and obscenity are not permitted ",
+              "A minimum of 6 and a maximum of 30 members inclusive of backstage helpers will be allowed to perform on the stage. ",
+              "Marks will be allotted based on the Performance, Team coordination and Costume selection. ",
+              "Any damage caused to the stage or any other device will be viewed seriously. ",
+              "Judge's and organizer's decisions will be final. "
             ]
           }
         ],
         registration: [
-          "reg The work will be judged based on creativity, incase of tie likes will      be considered.",
-          "The entries shouldn’t have any watermark.",
-          " The entries should be submitted during registration.",
-          "Identification of plagiarism will lead to disqualification",
-          " The judge's decision will be final"
+          "College ID is mandatory.",
+          "The entry fee is Rs.220 per head.",
+          "Bonafide should be submitted.",
+          "Registration has to be done online and this event comes under premium registration.",
+          "Only a limited number of slots are available..",
+          "Participants are requested to make their presence inside the college premises before 2 pm.",
+          "The team-head should register for the event, on behalf of the team."
         ],
         contact: [
-          { name: "Person", number: "1234567890" },
-          { name: "Person", number: "1234567890" }
+          { name: "Kavya", number: "+91 6374253694" },
+          { name: "Akshaya Preetha V", number: "+91 9361100381" }
         ],
         link: "https://www.google.com",
-        image: "tklogo"
+        image: "choreonight"
       },
       {
-        name: "event1",
+        name: "Musical night",
         description:
-          "A photo is a moment frozen forever in time. Show the life you've lived through the lens and the moments you've preserved through your clicks.",
-        rules: [
-          "The work will be judged based on creativity, incase of tie likes will      be considered.",
-          "The entries shouldn’t have any watermark.",
-          " The entries should be submitted during registration.",
-          "Identification of plagiarism will lead to disqualification",
-          " The judge's decision will be final"
+          "Let the music levitate your soul. The stage is set ablaze with melodies to vibearound..",
+        registration: [
+          "College ID card is mandatory",
+          "It comes under general registration"
         ],
         contact: [
           { name: "Person", number: "1234567890" },
           { name: "Person", number: "1234567890" }
         ],
         link: "https://www.google.com",
-        image: "tklogo"
+        image: "musicalnight"
       },
       {
-        name: "event1",
+        name: "Folk night",
         description:
-          "A photo is a moment frozen forever in time. Show the life you've lived through the lens and the moments you've preserved through your clicks.",
-        rules: [
-          "The work will be judged based on creativity, incase of tie likes will      be considered.",
-          "The entries shouldn’t have any watermark.",
-          " The entries should be submitted during registration.",
-          "Identification of plagiarism will lead to disqualification",
-          " The judge's decision will be final"
+          "It's time to break the silence of the night with some blasting folk music! Get ready folks, the night is ours to rule!.",
+        registration: [
+          "College ID card is mandatory",
+          "It comes under general registration"
         ],
         contact: [
           { name: "Person", number: "1234567890" },
           { name: "Person", number: "1234567890" }
         ],
         link: "https://www.google.com",
-        image: "tklogo"
+        image: "folknight"
       },
       {
-        name: "event1",
+        name: "DJ night",
         description:
-          "A photo is a moment frozen forever in time. Show the life you've lived through the lens and the moments you've preserved through your clicks.",
-        rules: [
-          "The work will be judged based on creativity, incase of tie likes will      be considered.",
-          "The entries shouldn’t have any watermark.",
-          " The entries should be submitted during registration.",
-          "Identification of plagiarism will lead to disqualification",
-          " The judge's decision will be final"
+          "Capturing the feeling of a crowd Rocking out to an awesome DJ. We'll be burning up like neon lights, Wild for the night.",
+        registration: [
+          "College ID card is mandatory",
+          "It comes under general registration"
         ],
         contact: [
           { name: "Person", number: "1234567890" },
           { name: "Person", number: "1234567890" }
         ],
         link: "https://www.google.com",
-        image: "tklogo"
-      },
-      {
-        name: "event1",
-        description:
-          "A photo is a moment frozen forever in time. Show the life you've lived through the lens and the moments you've preserved through your clicks.",
-        rules: [
-          "The work will be judged based on creativity, incase of tie likes will      be considered.",
-          "The entries shouldn’t have any watermark.",
-          " The entries should be submitted during registration.",
-          "Identification of plagiarism will lead to disqualification",
-          " The judge's decision will be final"
-        ],
-        contact: [
-          { name: "Person", number: "1234567890" },
-          { name: "Person", number: "1234567890" }
-        ],
-        link: "https://www.google.com",
-        image: "tklogo"
+        image: "djnight"
       }
     ]
   },
@@ -118,58 +95,167 @@ export const culturalEventsData = [
     heading: "Events",
     data: [
       {
-        name: "night event1",
+        name: "Pitch Perfect",
         description:
-          "A photo is a moment frozen forever in time. Show the life you've lived through the lens and the moments you've preserved through your clicks.",
+          "Light up the souls and charm them with your voice. Let your melody touch their heartstrings with your ethereal voice..",
+        registration: [
+          "College ID is mandatory.",
+          "The event comes under general registration."
+        ],
         rules: [
-          "The work will be judged based on creativity, incase of tie likes will      be considered.",
-          "The entries shouldn’t have any watermark.",
-          " The entries should be submitted during registration.",
-          "Identification of plagiarism will lead to disqualification",
-          " The judge's decision will be final"
+          {
+            "": [
+              "Usage of metronomes will not be permitted.",
+              "(Device used by musicians that marks time at a selected rate by giving a regular tick.)",
+              "Performance can be of any language.",
+              "Two rounds - prelims and finals."
+            ]
+          },
+          {
+            Prelims: [
+              "Each participant will be given 1 to 2 minutes including setup time.",
+              "Karaoke is not allowed.",
+              "Participants will not be allowed to refer to lyrics while singing.",
+              "There will be no instrumental accompaniment during the prelims.",
+              "Judges’ and organizers' decisions will be final."
+            ]
+          },
+          {
+            Finals: [
+              "Finals will take place once the prelims results are announced..",
+              "Time limit is 3 to 4 minutes.",
+              "Referring to lyrics is not allowed.",
+              "There will be no instrumental accompaniments during the finals..",
+              "Judges and organizers’ decisions will be final."
+            ]
+          }
         ],
         contact: [
-          { name: "Person", number: "1234567890" },
-          { name: "Person", number: "1234567890" }
+          { name: "Sai Teja", number: "9390165105" },
+          { name: "Hariharan", number: "7395985798" }
         ],
         link: "https://www.google.com",
         image: "tklogo"
       },
       {
-        name: "event1",
+        name: "Acapella",
         description:
-          "A photo is a moment frozen forever in time. Show the life you've lived through the lens and the moments you've preserved through your clicks.",
+          "Let the voice of your harmony stir the souls of the audience! Make your melody march to victory with the triumphant tunes of your team!.",
+        registration: [
+          "College ID is mandatory.",
+          "The event comes under general registration."
+        ],
         rules: [
-          "The work will be judged based on creativity, incase of tie likes will      be considered.",
-          "The entries shouldn’t have any watermark.",
-          " The entries should be submitted during registration.",
-          "Identification of plagiarism will lead to disqualification",
-          " The judge's decision will be final"
+          {
+            Prelims: [
+              "Open theme",
+              "Timing should not exceed 3 minutes.",
+              "A minimum of 2 and a maximum of 4 participants are allowed per team..",
+              "Only one musical instrument is allowed..",
+              "Karaoke is not allowed.",
+              "Unparliamentary words should be avoided .",
+              "Judge's and organizer's decisions will be final.",
+              "5 students will be selected for the final round."
+            ]
+          },
+          {
+            Finals: [
+              "Participants should sing without instruments, only vocals..",
+              "Timing should not exceed 2 minutes.",
+              "10 mins will be given for preparation.."
+            ]
+          }
         ],
         contact: [
-          { name: "Person", number: "1234567890" },
-          { name: "Person", number: "1234567890" }
+          { name: "Sai Teja", number: " 9390165105" },
+          { name: "Hariharan", number: "7395985798" }
         ],
         link: "https://www.google.com",
-        image: "tklogo"
+        image: "acapella"
       },
       {
-        name: "event1",
+        name: "60 To Victory",
         description:
-          "A photo is a moment frozen forever in time. Show the life you've lived through the lens and the moments you've preserved through your clicks.",
+          "60 seconds is all that it takes to win the words, give your best for Just -A- minute without hesitation and deviation but with sheer dedication and the game is yours. . ",
+        registration: [
+          "College ID is mandatory.",
+          "The event comes under general registration."
+        ],
         rules: [
-          "The work will be judged based on creativity, incase of tie likes will      be considered.",
-          "The entries shouldn’t have any watermark.",
-          " The entries should be submitted during registration.",
-          "Identification of plagiarism will lead to disqualification",
-          " The judge's decision will be final"
+          {
+            "Round 1": [
+              "The speakers are only permitted to speak in English for both the rounds",
+              "Usage of unparliamentary words leads to immediate disqualification.",
+              "Fillers must not be used. ",
+              "The topic will be given on the spot. ",
+              "No repetitive words or ideas..",
+              "Objection by the judge is allowed.",
+              "Participants must follow the general rules set by the judge while performing.",
+              "Judge's and organizer's’ decisions will be final."
+            ]
+          },
+          {
+            Finals: [
+              "Block and tackle",
+              "Each speaker should speak for 1 minute without any interruption..",
+              "Speakers will be given 30 seconds for preparing the topic once they enter the stage.",
+              "Topics given will differ from one participant to another..",
+              "Other participants cannot interrupt the current speaker..",
+              `While speaking, the participant should not do the following: \n 1. Hesitating ( ex: usage of fillers and very slow speaking) \n 2. Repetition of the same sentences. \n 3. Deviation from the given topic (Irrelevance).`,
+              "Points will be given based on fluency and the judging criteria..",
+              "Judge's and organizer's’ decisions will be final.."
+            ]
+          }
         ],
         contact: [
-          { name: "Person", number: "1234567890" },
-          { name: "Person", number: "1234567890" }
+          { name: "Sanjana", number: "7397314380" },
+          { name: "Dharun", number: "7358609797" }
         ],
         link: "https://www.google.com",
-        image: "tklogo"
+        image: "60tovictory"
+      },
+      {
+        name: "Tk Tv",
+        description:
+          "With a click of the button, the channel changes; With a pick of words, your topic changes too! Act out the given topic, on-spot and show off your creativity!.",
+        registration: [
+          "College ID is mandatory.",
+          "The event comes under general registration."
+        ],
+        rules: [
+          {
+            "": [
+              "A random team of 2 to 4 members will be made with the participants.",
+              "Usage of mobiles and other gadgets is not allowed.",
+              "Vulgarity in any form will lead to elimination. ",
+              "Semi controversial topics can be chosen just for the purpose of humor. ",
+              " Expressing any sort of opinion with respect to politics, religion and other sensitive topics are not allowed.",
+              "This event comprises two levels.",
+              "Judges’ and organizers’ decisions will be final."
+            ]
+          },
+          {
+            "Round 1": [
+              "Channel surfing",
+              "Participants must switch to different channels on the command of the judge. ",
+              "Channels include languages such as Hindi, Tamil, Telugu, Malayalam and English. ",
+              " Teams that qualify this round will be eligible to take part in the next round.."
+            ]
+          },
+          {
+            Finals: [
+              "Adzap.",
+              "Time allotted for preparation is one minute..",
+              "Maximum time for performance is 3 minutes."
+            ]
+          }
+        ],
+        contact:[
+          {name:"Supriya", number:"9390041226"},
+          {name:"Hariharan", number:"7395985798"}
+          ],
+        link: "https://www.google.com",
+        image: "tktv"
       }
     ]
   },
