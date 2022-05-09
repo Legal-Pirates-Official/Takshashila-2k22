1dba9cad84b129f613e710fdcd7000398afe9c6a
s-ashwin <ashwin.sivanagarajan@gmail.com>
2022-05-09 13:10:10 +0530

    Updates partial content

--- src/assets/Events/CulturalEventsData.js
+++ src/assets/Events/CulturalEventsData.js
@@ -250,72 +250,306 @@ export const culturalEventsData = [
             ]
           }
         ],
-        contact:[
-          {name:"Supriya", number:"9390041226"},
-          {name:"Hariharan", number:"7395985798"}
-          ],
+        contact: [
+          { name: "Supriya", number: "9390041226" },
+          { name: "Hariharan", number: "7395985798" }
+        ],
         link: "https://www.google.com",
         image: "tktv"
-      }
-    ]
-  },
-  {
-    heading: "Online Events",
-    data: [
-      {
-        name: "night event1",
-        description:
-          "A photo is a moment frozen forever in time. Show the life you've lived through the lens and the moments you've preserved through your clicks.",
-        rules: [
-          "The work will be judged based on creativity, incase of tie likes will      be considered.",
-          "The entries shouldn’t have any watermark.",
-          " The entries should be submitted during registration.",
-          "Identification of plagiarism will lead to disqualification",
-          " The judge's decision will be final"
-        ],
-        contact: [
-          { name: "Person", number: "1234567890" },
-          { name: "Person", number: "1234567890" }
-        ],
-        link: "https://www.google.com",
-        image: "tklogo"
       },
       {
-        name: "event1",
+        name: "It’s Debatable",
         description:
-          "A photo is a moment frozen forever in time. Show the life you've lived through the lens and the moments you've preserved through your clicks.",
+          "Prove your wits and convince them with your words. This is your destiny, you have been arguing for. For what's worth, debate for it!",
+        registration: [
+          "College ID is mandatory.",
+          "The event comes under general registration.",
+          "The team-head should register for the event, on behalf of the team."
+        ],
         rules: [
-          "The work will be judged based on creativity, incase of tie likes will      be considered.",
-          "The entries shouldn’t have any watermark.",
-          " The entries should be submitted during registration.",
-          "Identification of plagiarism will lead to disqualification",
-          " The judge's decision will be final"
+          {
+            "": [
+              "Each team should have a maximum of 3 members.",
+              "Exchanging thoughts should be only in English (usage of other languages is not allowed) ",
+              "Topic will be provided on the spot. ",
+              "Participants will be given 3 minutes to prepare on the topic specified.",
+              "There will be two rounds conducted in this event. ",
+              "Usage of abusive words is not permitted.",
+              "Participants are not allowed to cross talk.",
+              "Abusing your opponent personally is strictly prohibited.",
+              "Judges’ and organizers’ decisions will be final."
+            ]
+          }
         ],
         contact: [
-          { name: "Person", number: "1234567890" },
-          { name: "Person", number: "1234567890" }
+          { name: "Sanjana", number: "7397314380" },
+          { name: "Gunalan", number: "8610822039" }
         ],
         link: "https://www.google.com",
-        image: "tklogo"
+        image: "itsdebatable"
       },
       {
-        name: "event1",
+        name: "24 Frames",
         description:
-          "A photo is a moment frozen forever in time. Show the life you've lived through the lens and the moments you've preserved through your clicks.",
+          "It's not a youtube short or reel. Let the beauty of your short film sink through our minds. Open our eyes , oh director",
+        registration: [
+          "College ID is mandatory.",
+          "The event comes under general registration.",
+          "The team-head should register for the event, on behalf of the team."
+        ],
         rules: [
-          "The work will be judged based on creativity, incase of tie likes will      be considered.",
-          "The entries shouldn’t have any watermark.",
-          " The entries should be submitted during registration.",
-          "Identification of plagiarism will lead to disqualification",
-          " The judge's decision will be final"
+          {
+            "": [
+              "Participants are requested to send their short films to the google drive..",
+              "Submissions should be sent on or before the 18th MAY 2022.",
+              "Shortlisted films will be screened on the day of the event.",
+              "Poster and teaser will be required for short films ",
+              "Controversial, sensitive and abusive contents are not encouraged.",
+              "Judges’ and organizers’ decisions will be final."
+            ]
+          }
         ],
         contact: [
-          { name: "Person", number: "1234567890" },
-          { name: "Person", number: "1234567890" }
+          { name: "Supriya", number: "9390041226" },
+          { name: "Chandru", number: "9962737406" }
         ],
         link: "https://www.google.com",
-        image: "tklogo"
-      }
+        image: "24frames"
+      },
+      {
+        name: "Act-O-Mania",
+        description:
+          "Silence is better than a thousand words. In this utopia of expressions and gestures let your emotions come to life and your actions reign over the spotlight.. It's not a youtube short or reel. Let the beauty of your short film sink through our minds. Open our eyes , oh director",
+        registration: [
+          "College ID is mandatory.",
+          "The event comes under general registration.",
+          "The team-head should register for the event, on behalf of the team."
+        ],
+        rules: [
+          {
+            "": [
+              "Each team can have a maximum of 4 to 10 members.",
+              "Duration of performing the act is limited from 3 to 6 minutes for each team.",
+              "Exceeding the specified time is strictly prohibited.",
+              "Teams selected from the group stage will be allowed to perform on the informal stage.",
+              "The music chosen by the teams must be handed over to the event organizer 30 mins before the commencement of the event. ",
+              "Usage of vulgar gestures will lead to elimination..",
+              "Acts based on controversial and sensitive topics are not permitted.",
+              "Judges’ and organizers’ decisions will be final."
+            ]
+          }
+        ],
+        contact: [
+          { name: "Supriya", number: "9390041226" },
+          { name: "Hariharan", number: "7395985798" }
+        ],
+        link: "https://www.google.com",
+        image: "actomania"
+      },
+      {
+        name: "Solo Dance",
+        description:
+          "Sway your elite feet and match em with the beats.lose yourself to the melody and let the dance display the real you.",
+        registration: [
+          "College ID is mandatory.",
+          "The event comes under general registration."
+        ],
+        rules: [
+          {
+            "": [
+              " Participants can dance to any song of their choice",
+              "The time limit is 3 minutes. ",
+              "The participants are requested to bring a USB drive with the music and submit it to the coordinators before the event starts. ",
+              "Participants should get their own props.",
+              " All props must be specified to the event coordinator. ",
+              "The lighting of matchsticks, candles, and cigarettes, or any derogatory acts are not allowed on stage.",
+              "Any obscene/offensive music/song is not allowed. ",
+              "The judges' decisions will be final."
+            ]
+          }
+        ],
+        contact: [
+          { name: "Ganapathi", number: "9344267027" },
+          { name: "Abhirami", number: "7358417463" }
+        ],
+        link: "https://www.google.com",
+        image: "solodance"
+      },
+      {
+        name: "Duals",
+        description:
+          "Synchronize your moves , it's just a dance for two , dual like never before else it's just another deja-vu",
+        registration: [
+          "College ID is mandatory.",
+          "The event comes under general registration.",
+          "The team-head should register for the event, on behalf of the team."
+        ],
+        rules: [
+          {
+            "": [
+              "Participants can dance to any song of their choice.",
+              "The time limit is 3 minutes",
+              "A team of only two is permitted. ",
+              "The participants are requested to bring a USB drive with the music and submit it to the coordinators before the event starts.",
+              "The participants will be judged based on their costumes, visual presentation, and dance form. ",
+              "There is no particular theme. ",
+              "Participants should get their own props. All props must be specified to the event coordinator. ",
+              "The lighting of matchsticks, candles, and cigarettes, or any derogatory acts are strictly not allowed on stage. ",
+              "Crossover teams are permitted.( participants can be from different colleges) ",
+              "Any obscene/offensive music/song is not allowed.",
+              "The judges' decision will be final."
+            ]
+          }
+        ],
+        contact: [
+          { name: "Manasa", number: "9490509472" },
+          { name: "Dharahash", number: "8072055008" }
+        ],
+        link: "https://www.google.com",
+        image: "duals"
+      },
+      {
+        name: "BGMI",
+        description:
+          "Zero down your enemies, unshackle the beast within you and establish your dominance. Prove yourself as the undefeated champion , it's game on! Kill. Reload. Repeat.",
+        registration: [
+          "College ID is mandatory.",
+          "The event comes under general registration.",
+          "The team-head should register for the event, on behalf of the team."
+        ],
+        rules: [
+          {
+            "": [
+              "Gaming should be done only using mobile phones. ",
+              "Emulators and Ipads are not allowed.",
+              "All the members of the squad should register themselves in advance.",
+              "Involvement of unregistered members will lead to instant elimination of the squad.",
+              "Players should have all the maps downloaded prior to the event.",
+              "No refund will be provided if the player is kicked out of the server due to glitches or any other cause..",
+              "In case of any technical issues in the event, the match will be rescheduled. Participants are asked to agree with the new time allotted.",
+              "Matches will be supervised separately to avoid malpractices."
+            ]
+          }
+        ],
+        contact: [
+          { name: "Sathiyaramanan", number: "9500206795" },
+          { name: "Aswath", number: "8946028066" }
+        ],
+        link: "https://www.google.com",
+        image: "bgmi"
+      },
+      {
+        name: "Funzone",
+        description:
+          "Suit up and put your game faces on as you're about to dive into our world of mini games-the fun zone.",
+        contact: [
+          { name: "Person", number: "0000" },
+          { name: "Person", number: "0000" }
+        ],
+        link: "https://www.google.com",
+        image: "funzone"
+      },
+      {
+        name: "Behind Yellow Tapes",
+        description:
+          "Alas, it has come to our eyes, a poor soul has been put to sleep ruthlessly. Is there any Sherlock in the town ? who can bring justice to the departed, with wits and clues and conclude the case to rest in peace.",
+        registration: [
+          "College ID is mandatory.",
+          "The event comes under general registration.",
+          "The team-head should register for the event, on behalf of the team."
+        ],
+        rules: [
+          {
+            "": [
+              "Time allotted for each team is 10 min.",
+              "Each team must consist of 3 to 4 members.",
+              "Team will be a time of 3 to 4 minutes gto inspect the crime scene.",
+              "A video of suspects statements will be played to the teams.",
+              "Team must discuss the case within the specified time.",
+              "Gadgets will not be allowed within premises.",
+              "For each hint asked by the team accountable points will be deducted as per jury team.",
+              "Points shall be awarded based on different criteria and shall be informed beforehand."
+            ]
+          }
+        ],
+        contact: [
+          { name: "Sai Teja", number: "9390165105" },
+          { name: "Vishalakshi Janani", number: "8838290451" }
+        ],
+        link: "https://www.google.com",
+        image: "behindyellowtapes"
+      },
+      {
+        name: "Meme Craft",
+        description:
+          "Let your crazy thoughts create viral sensation and hype the trend, Showcase it to create a trendy bloom.Let the Meme Kombat begin!.",
+        registration: [
+          "College ID is mandatory.",
+          "The event comes under general registration."
+        ],
+        rules: [
+          {
+            "Round 1": [
+              "The memes submitted should be the original work of the individual.",
+              "Submissions should be sent on or before the 18th MAY 2022.",
+              "Upload your meme by visiting our website link www.cittakshashila.org",
+              " First-round will be an online round where participants need to upload their memes to their respective google forms within the deadline.",
+              "Selection will be done on the basis of creativity and originality of the meme.",
+              "Judging criteria 50% will be based on creativity and 50% based on likes."
+            ]
+          },
+          {
+            "Round 2": [
+              "Final round will be conducted offline and participants have to create a meme on the spot.",
+              "Theme and Templates for the final round will be provided by the organizers.",
+              "Decision of the jury panel will be final."
+            ]
+          }
+        ],
+        contact: [
+          { name: "Rahima", number: "9003923429" },
+          { name: "Priyadharshini", number: "8870528203" }
+        ],
+        link: "https://www.google.com",
+        image: "memecraft"
+      },
+      {
+        name: "Stylus Strokes",
+        description:
+          "Craft an art that steals our heart.Don't limit yourself to an empty artwork.Design like a pro, things just got digital.",
+        registration: [
+          "College ID is mandatory.",
+          "The event comes under general registration."
+        ],
+        rules: [
+          {
+            "": [
+              "Only one entry is permitted.",
+              "Upload your digital art by visiting our website link www.cittakshashila.org",
+              "Work should be submitted in an A4 sized canvas.",
+              "Offensive content must be avoided.",
+              "Submissions should not contain any watermarks.",
+              "Overtracing images from google should be avoided.",
+              "Incase of plagiarism, entries will be disqualified.",
+              `The below mentioned softwares should only be used for crafting:
+                  1. Adobe photoshop
+                  2. Adobe Illustrator
+                  3. Krita
+                  4. Sketchbook
+                  5. Affinity
+                  6. Rebelle
+                  7. Procreate`,
+              "Judging criteria 50% will be based on creativity and 50% based on likes."
+            ]
+          }
+        ],
+        contact: [
+          { name: "Poorvaja", number: "9789934951" },
+          { name: "Jansi Victoria", number: "7904060230" }
+        ],
+        link: "https://www.google.com",
+        image: "stylusstrokes"
+      },
     ]
   },
   {
